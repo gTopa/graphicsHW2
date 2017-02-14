7350a6bad5764be50942e92bd680b5697ba6f524
gTopa <gtopa@stuy.edu>
2017-02-14 15:34:47 -0500

    work

--- draw.py
+++ draw.py
@@ -1,40 +1,54 @@
 from display import *
 
 def draw_line( screen, x0, y0, x1, y1, color ):
+    if(x0>x1):
+        xTemp=x0
+        x0=x1
+        x1=xTemp
+        yTemp=y0
+        y0=y1
+        y1=yTemp
     A=y1-y0
     B=-(x1-x0)
-    if(A<=-B and A>=0 and B<0):
-        x=x0
-        y=y0
+    x=x0
+    y=y0
+    if(A<=-B and A>=0 and B<0): #Octant 1
         d=2*A+B
-        while (x<=x1):
+        print "1"
+        while (x<x1):
             plot(screen,color,x,y)
             if (d>0):
                 y+=1
                 d+=2*B
             x+=1
             d+=2*A
-    if(A>-B and A>0 and B<=0):
-        x=y0
-        y=x0
+    if(A>-B and A>0 and B<=0): #Octant 2
         d=2*B+A
-        while (x<=x1):
+        print "2"
+        while (y<y1):
             plot(screen,color,x,y)
-            if (d>0):
-                y+=1
+            if (d<0):
+                x+=1
                 d+=2*A
-            x+=1
+            y+=1
             d+=2*B
-    if(A<B and A<0 and B<0)
-        x=x0
-        y=y0
-        d=2*A-B
-        while (x<=x1):
+    if(A<=B and A<0 and B<0): #Octant 7
+        d=A-2*B
+        print "7"
+        while (y>y1):
             plot(screen,color,x,y)
             if (d>0):
+                x+=1
+                d+=2*A
+            y-=1
+            d-=2*B
+    if(A>B and A<0 and B<0): #Octant 8
+        d=2*A-B
+        print "8"
+        while(x<x1):
+            plot(screen, color, x, y)
+            if(d<0):
                 y-=1
                 d-=2*B
             x+=1
             d+=2*A
-        
-    
--- main.py
+++ main.py
@@ -4,16 +4,38 @@ from draw import *
 screen = new_screen()
 color = [ 0, 255, 0 ]
 
-draw_line(screen, 3, 3, 100, 10, color)
+draw_line(screen, 250, 250, 350, 300, color)
 
 display(screen)
 
-draw_line(screen, 3, 200, 150, 250, color)
+
+draw_line(screen, 250, 250, 350, 200, color)
 
 display(screen)
 
-draw_line(screen, 3, 300, 80, 377, color)
+'''draw_line(screen, 250, 250, 150, 200, color)
+
+display(screen)
+
+draw_line(screen, 250, 250, 350, 200, color)
+
+display(screen)
+
+draw_line(screen, 250, 250, 300, 350, color)
+
+display(screen)
+
+draw_line(screen, 250, 250, 300, 150, color)
+
+display(screen)
+
+draw_line(screen, 250, 250, 200, 150, color)
+
+display(screen)
+
+draw_line(screen, 250, 250, 200, 350, color)
 
 display(screen)
 
 save_extension(screen, 'img.png')
+'''
